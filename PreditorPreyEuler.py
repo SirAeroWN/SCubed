87f1a576ccf96f140d3e245b344b988ca9db0473
Will Norvelle <willnorvelle@gmail.com>
2015-04-24 08:47:03 -0400

    It

--- PreditorPreyEuler.py
+++ PreditorPreyEuler.py
@@ -6,31 +6,38 @@ from matplotlib.pylab import *
 import numpy as np
 
 def rabbitChange(rabbitsP, foxesP):
-	return (0.8*rabbitsP - 0.04*rabbitsP*foxesP)
+	print(0.8*rabbitsP - (0.04 * rabbitsP * foxesP))
+	return (0.8*rabbitsP - (0.04 * rabbitsP * foxesP))
 
 def newRabbits(currentRabbits, currentFoxes):
-	return (currentRabbits + rabbitChange(currentRabbits, currentFoxes)*deltaT)
+	return (currentRabbits + (rabbitChange(currentRabbits, currentFoxes) * deltaT))
 
 def foxChange(rabbitsP, foxesP):
-	return (-0.3*foxesP + 0.006*rabbitsP*foxesP)
+	return (-0.3*foxesP + (0.006 * rabbitsP * foxesP))
 
 def newFoxes(currentRabbits, currentFoxes):
-	return (currentFoxes + foxChange(currentRabbits, currentFoxes)*deltaT)
+	return (currentFoxes + (foxChange(currentRabbits, currentFoxes) * deltaT))
 
 foxes = 10
 rabbits = 55
 t = 0
-deltaT= 1
+deltaT= 0.1
 rabbity = np.empty(1000)
 foxy = np.empty(1000)
-x = np.arange(0, 1000, deltaT)
+x = np.arange(0, 100, deltaT)
 for i in range(0, 1000):
-	temp = getNewY(Y)
-	y[i] = temp
-	Y = temp
-	X += deltaT
-plot(x,y)
+	newRab = newRabbits(rabbits, foxes)
+	newFox = newFoxes(rabbits, foxes)
+	rabbity[i] = newRab
+	foxy[i] = newFox
+	rabbits = newRab
+	foxes = newFox
+	t += deltaT
+print(rabbity)
+plot(x,rabbity)
+plot(x,foxy)
+axis([0, 100, amin(foxy), amax(rabbity)])
 xlabel('t')
-ylabel('p')
-legend('dp/dt = 3p(1-p)')
+ylabel('Foxes and Rabbits')
+legend('')
 show()
